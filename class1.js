28bbad09e7fc3820c41e9b1aa35e1c28c71e1449
sarah.hakeam <67658131+sarah-abdelhakeam@users.noreply.github.com>
2023-05-28 09:05:18 +0300

    Add files via upload

---
+++ class1.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class Greating extends Component{
+
+    render()
+    {
+        return(
+            <h1>
+                welcome, iot track
+            </h1>
+        )
+    }
+}
+export default Greating;
